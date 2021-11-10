7c7ebe4a1e0174ded89be6653de7122288e32af5
darkseear <38041141+darkseear@users.noreply.github.com>
2021-11-10 04:40:43 +0300

    read_one - добавил карту категорий и выдел товара

--- src/components/pages/read_one/ReadOne.jsx
+++ src/components/pages/read_one/ReadOne.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { getReadOne } from '../../../redux/reducers/product_reducer'
+import Categories from '../../web_components_kit/categories/Categories'
 
 function ReadOne() {
 
@@ -16,9 +17,15 @@ function ReadOne() {
 
     return (
         <div>
-            {
-                read_one && read_one !== undefined  && "id:" + read_one.id + "____" + read_one.name
-            }
+             <div style={{ float:'left', marginRight:'25px'}}>
+                    <Categories />
+            </div>
+            <div>
+                {
+                    read_one && read_one !== undefined  && "id:" + read_one.id + "____" + read_one.name
+                }  
+            </div>
+          
         </div>
     )
 }
