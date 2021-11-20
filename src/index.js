e760bf74b494c49fe48e66659733ec7134b69485
darkseear <38041141+darkseear@users.noreply.github.com>
2021-11-20 12:51:45 +0300

    поддержка в ie 11

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
        <App />
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
