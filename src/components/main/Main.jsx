76e5fd89f206e215a676701e46d7efc52e784ed6
darkseear <38041141+darkseear@users.noreply.github.com>
2021-12-14 20:48:54 +0300

    категории в верхней панели

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ContentBody from '../content_body/ContentBody'
 import Categories from '../web_components_kit/categories/Categories'
+import CategoriesTop from '../web_components_kit/categories/CategoriesTop'
 import GoBackNext from '../web_components_kit/goBack/GoBackNext'
 
 function Main({ isLogin, setIsLogin }) {
     return (
         <main>
+            <CategoriesTop />
            <ContentBody isLogin={isLogin} setIsLogin={setIsLogin}/>
         </main>
     )
---
+++ src/components/web_components_kit/categories/CategoriesTop.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { category } from '../../../redux/reducers/category_reducer'
+
+
+function CategoriesTop() {
+
+    const dispatch = useDispatch()
+    const { categoryArr } = useSelector(state => state.categorys)
+
+    useEffect(() => {
+        dispatch(category())
+    }, [])
+
+    return (
+        <div className="categoryTop_container">
+            {
+                categoryArr !== null && categoryArr !== undefined ? categoryArr.map((item) => <NavLink className="categoryTop_element" style={{ color: 'black' }} activeClassName="category_navigation_active" to={`/catalog/${item.id}`} key={item.id}>
+                   
+                        {item.name}
+                    
+                </NavLink>)
+                    :
+                    <div>Loading ... </div>
+
+            }
+        </div>
+    )
+}
+
+export default CategoriesTop
