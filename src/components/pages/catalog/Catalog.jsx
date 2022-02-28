52236862d756b1ba962384a2292ef1a8f9b25c4c
darkseear <darkseeara@yandex.ru>
2022-02-28 17:03:56 +0300

    useMemo

--- src/components/pages/catalog/Catalog.jsx
+++ src/components/pages/catalog/Catalog.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
@@ -13,30 +14,36 @@ function Catalog() {
     const dispatch = useDispatch()
     const { id } = useParams()
 
-    useEffect(()=>{
+    useMemo(()=>{
         dispatch(products(id))
     }, [id])
 
-    const { products_category_id } = useSelector( state => state.products )
+    // useEffect(() => {
+    //    specFunction()
+    // }, [id])
+
+    const { products_category_id } = useSelector(state => state.products)
 
     return (
         <>
             <div className="catalog_container" >
-                <PageTitle  title={"Каталог"} />
+                <PageTitle title={"Каталог"} />
                 <div className="catalog_container-content __container">
-                    <div style={{ float:'left', marginRight:'25px'}}>
+                    <div style={{ float: 'left', marginRight: '25px' }}>
                         <Categories />
                     </div>
                     <div className="catalog-page_container">
-                        { products_category_id && products_category_id !== undefined && products_category_id !== null ? 
-                    <ProductPage products_category_id={products_category_id}/> 
-                        :
-                        <div>Loading...</div>
+                        {products_category_id &&
+                         products_category_id !== undefined &&
+                         products_category_id !== null ?
+                            <ProductPage products_category_id={products_category_id} />
+                            :
+                            <div>Loading...</div>
                         }
                     </div>
                 </div>
             </div>
-            <GoBackNext/>
+            <GoBackNext />
         </>
     )
 }
